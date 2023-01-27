d5fadbc76f6c599ec98812f55a731b4e37c09e05
Dima <dimafrontbackdev@gmail.com>
2023-01-27 21:29:03 +0200

    burger z-index fix

--- src/assets/js/burgerMenu.js
+++ src/assets/js/burgerMenu.js
@@ -4,15 +4,21 @@ const burgerMenu = () => {
   const closeBurger = document.querySelector('.close-burger');
   const burgerMenu = document.querySelector('.burger-menu');
   const headerNavItems = document.querySelectorAll('.header__nav-item');
+  const burgerMenuTransition = 700; // 0.7
 
   const openBurgerMenu = () => {
     burgerMenu.classList.add('burger-menu--active');
+    burgerMenu.style.zIndex = '5';
     document.body.classList.add('overflow-h');
   };
 
   const closeBurgerMenu = () => {
     burgerMenu.classList.remove('burger-menu--active');
     document.body.classList.remove('overflow-h');
+
+    setTimeout(() => {
+      burgerMenu.style.zIndex = '-1';
+    }, burgerMenuTransition);
   };
 
   burger.addEventListener('click', openBurgerMenu);
